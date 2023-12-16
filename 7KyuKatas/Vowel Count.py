c13b53748f7602022a3e42e617a1628e357d59fc
HM237 <150607364+HM237@users.noreply.github.com>
2023-12-16 20:31:39 +0000

    Create Vowel Count.py

---
+++ 7KyuKatas/Vowel Count.py
@@ -0,0 +1,7 @@
+def get_count(sentence):
+    a = sentence.count("a")
+    e = sentence.count("e")
+    i = sentence.count("i")
+    o = sentence.count("o")
+    u = sentence.count("u")
+    return a + e + i + o + u
