f535bb329586665fdd9ab623e9262adcb9559bf5
HM237 <150607364+HM237@users.noreply.github.com>
2024-01-11 21:23:36 +0000

    Create IPv4 to int32.py

---
+++ 6KyuKatas/IPv4 to int32.py
@@ -0,0 +1,13 @@
+def ip_to_int32(ip):
+  arr = ip.split(".")
+  binary_arr = []
+  for i in arr:
+      binary = bin(int(i))
+      binary_value = str(binary[2:])
+      if len(binary_value) < 8 : 
+        binary_value= binary_value.zfill(8)
+      binary_arr.append(binary_value)
+  print(binary_arr)
+  binary = ("".join(binary_arr))
+  decimal = int(binary, 2)
+  return decimal
