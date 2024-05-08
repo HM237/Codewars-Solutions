02b5fd3f4f86f904730ac9c77da6b088a6e36cd9
HM237 <150607364+HM237@users.noreply.github.com>
2024-05-08 13:01:56 +0100

    Update Sum of Digits or Digital Root.py

--- 6KyuKatas/Sum of Digits or Digital Root.py
+++ 6KyuKatas/Sum of Digits or Digital Root.py
@@ -1,7 +1,6 @@
 def digital_root(n):
     array = [int(i) for i in str(n)]
-    if len(array) == 1:
-        return n
+    if len(array) == 1: return n
     while len(array) > 1:
         array = sum(array)
         array = [int(i) for i in str(array)]
