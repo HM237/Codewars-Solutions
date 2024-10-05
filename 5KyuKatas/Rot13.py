b78c4e8df621ea3b507061ce72fed33e026137f8
HM7 <150607364+HM237@users.noreply.github.com>
2024-10-05 19:09:06 +0100

    Create Rot13.py

---
+++ 5KyuKatas/Rot13.py
@@ -0,0 +1,17 @@
+def rot13(message):
+    result = ""
+    if message:
+        for char in message:
+            if 65 <= ord(char) <= 90 or 97 <= ord(char) <= 122:
+                if 65 <= ord(char) <= 90:
+                    if ord(char) + 13 > 90:
+                        char = chr(ord(char) + 13 - 90 + 65 - 1)
+                    else:
+                        char = chr(ord(char) + 13)
+                else:
+                    if ord(char) + 13 > 122:
+                        char = chr(ord(char) + 13 - 122 + 97 - 1)
+                    else:
+                        char = chr(ord(char) + 13)
+            result += char
+    return result
